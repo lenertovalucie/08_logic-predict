755dc40d360b65b7d6421f90c05cd89cccd03fb6
Lucie Lénertová <lenertova.lucie@gmail.com>
2022-08-19 19:00:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# nespouštějte program!
+# jaké 2 řetězce se podle vás vytisknou do konzole? a proč?
+# vaši předpověď napište do komentářů níže
+#
+#
+
+name = "Dave"
+homeTown = "Seattle"
+
+if name == "Dave" and homeTown == "Seattle":
+    print("Hi there Dave from Seattle!")
+else:
+    print("You're not Dave from Seattle!")
+
+if name == "Dave" or homeTown == "Seattle":
+    print("You're either called Dave or you're from Seattle!")
+else:
+    print("You're not Dave, and you aren't from Seattle!")
